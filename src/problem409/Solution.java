6f5818915233d70ccb5b96d8ae4418c7b0eca816
Menezo <joaovitormenezesm12@gmail.com>
2025-01-24 22:24:44 -0300

    LeetCode #455 - Solution for Assign Cookies

--- src/problem409/Solution.java
+++ src/problem409/Solution.java
@@ -5,12 +5,13 @@ import java.util.HashSet;
 public class Solution {
 
     private static int findLongestPalindrome(String s) {
-
+        //HashSet to track the characters without pairs
         HashSet<Character> letters = new HashSet<>();
 
         int count = 0;
 
         for(Character ch : s.toCharArray()) {
+            //If the character is unmatched, adds it to the set. Otherwise, it's a pair, so remove it from the set and count the pair in the length
             if(!letters.contains(ch)) {
                 letters.add(ch);
             } else {
@@ -18,6 +19,7 @@ public class Solution {
                 count += 2;
             }
         }
+        //Verify if there's still any unmatched character on the set, if so it can be used as the center
         if(!letters.isEmpty()) {
             count++;
         }
---
+++ src/problem455/Solution.java
@@ -0,0 +1,28 @@
+package problem455;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        //Sort the arrays, so we can satisfy the least greedy children first
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        //Two pointers: child (g) and cookie (s)
+        int cookie = 0;
+        int child = 0;
+        int counter = 0;
+
+        //Go through the arrays till it reaches the end of one of them
+        while(child < g.length && cookie < s.length) {
+            //If so means that this cookie contents the child and go to the next one
+            if(g[child] <= s[cookie]) {
+                counter++;
+                child++; //Move to the next child only if the current child is content
+            }
+            //Always go to the next cookie 
+            cookie++;
+        }
+        return counter;
+    }
+}
